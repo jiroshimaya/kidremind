c7cda416353d18e377d5deb6adbb4b8b12682731
Jiro Shimaya <shimajiroxyz@users.noreply.github.com>
2024-08-17 23:50:33 +0900

    a

--- app.py
+++ app.py
@@ -91,4 +91,5 @@ def serve_image(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    debug = os.environ.get('FLASK_DEBUG', 'True').lower() == 'true'
+    app.run(debug=debug, host="0.0.0.0", port=port)
